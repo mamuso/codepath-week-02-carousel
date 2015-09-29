a4703dcff732e9d59f0b1fb63f73729414101383
Manuel Muñoz Solera <mamuso@mamuso.net>
2015-09-28 20:00:21 -0700

    PhotosViewController

---
+++ PhotosViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PhotosViewController.swift
+//  codepath-week-02-carousel
+//
+//  Created by mmuno on 9/28/15.
+//  Copyright © 2015 Manuel Muñoz Solera. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedImgeView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.contentSize = feedImgeView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
