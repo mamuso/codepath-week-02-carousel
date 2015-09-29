f566c4676e6a07589e9c4d1a19f7a78f0b0fda0a
Manuel Muñoz Solera <mamuso@mamuso.net>
2015-09-28 20:33:24 -0700

    Sign out, modal to root

---
+++ codepath-week-02-carousel/SettingsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewController.swift
+//  codepath-week-02-carousel
+//
+//  Created by mmuno on 9/28/15.
+//  Copyright © 2015 Manuel Muñoz Solera. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var settingsImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.frame.size.height = 500
+        scrollView.contentSize = settingsImageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didDissmissView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
